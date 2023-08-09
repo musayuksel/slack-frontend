ab1bb9a60160fbc7569a0bed68df766e2eb61570
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-09 19:57:17 +0100

    Add awd config and basic signup form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { config } from './aws_config';
 function App() {
+  const [userInfos, setUserInfos] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInfos((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.userPoolId,
+      ClientId: config.userPoolWebClientId,
+    });
+
+    userPool.signUp(
+      userInfos.email,
+      userInfos.password,
+      [],
+      [],
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (data) {
+            console.log('userName:', data.user.getUsername());
+          }
+        }
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={userInfos.email}
+          placeholder="email"
+          name="email"
+          type="email"
+        />
+        <input
+          onChange={handleChange}
+          value={userInfos.password}
+          placeholder="password"
+          name="password"
+          type="password"
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
---
+++ src/aws_config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  region: 'eu-west-2',
+  userPoolId: 'eu-west-2_VUdj4DoBW',
+  userPoolWebClientId: '229j8tmhe7ro5s6dtgcj2fa73e',
+};
