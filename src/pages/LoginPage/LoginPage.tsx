f86249b6bf0b78ca29240da478a428cca336d5b6
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-11 16:27:00 +0100

    Update Login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,76 @@
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserPool,
+} from 'amazon-cognito-identity-js';
+import { config } from '../../aws_config';
 
 export const LoginPage: FC = () => {
-  return <div>Login</div>;
+  const [userInfos, setUserInfos] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserInfos((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const userPool = new CognitoUserPool({
+    UserPoolId: config.userPoolId,
+    ClientId: config.userPoolWebClientId,
+  });
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const authenticationData = {
+      Username: userInfos.email,
+      Password: userInfos.password,
+    };
+
+    const authenticationDetails = new AuthenticationDetails(authenticationData);
+    const cognitoUser = new CognitoUser({
+      Username: userInfos.email,
+      Pool: userPool,
+    });
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => {
+        const accessToken = result.getAccessToken().getJwtToken();
+        console.log({ accessToken });
+      },
+      onFailure: (err) => {
+        console.error('Authentication failed:', err);
+        //TODO: Handle authentication failure
+      },
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={userInfos.email}
+          placeholder="Email"
+          name="email"
+          type="email"
+          autoComplete="email"
+        />
+        <input
+          onChange={handleChange}
+          value={userInfos.password}
+          placeholder="Password"
+          name="password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
