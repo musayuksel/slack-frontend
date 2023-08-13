5029388b359d70d0908a72c17dd3cb52666b0274
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-13 22:05:24 +0100

    Add logout

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,9 +3,16 @@ import { useNavigate } from 'react-router-dom';
 
 export const NavMenu: FC = () => {
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    navigate('/login');
+  };
+
   return (
     <nav>
       <button onClick={() => navigate('/')}>LOGO</button>
+      <button onClick={handleLogout}>Logout</button>
     </nav>
   );
 };
