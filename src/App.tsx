b8c2f2ffe6bfc3c8903fa662600f763d264a11f4
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-13 17:55:51 +0100

    Add dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { type FC } from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { HomePage, LoginPage, SignUpPage } from './pages';
+import { DashboardPage, HomePage, LoginPage, SignUpPage } from './pages';
 import { NavMenu } from './components/NavMenu';
 import './App.css';
 
@@ -12,6 +12,7 @@ const App: FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
   );
