a6887deb68679e9d24e845d0989dabc063ce3628
Musa <79163385+musayuksel@users.noreply.github.com>
2023-10-21 13:22:47 +0100

    Add initial channel messages

--- src/components/Channels/Channels.tsx
+++ src/components/Channels/Channels.tsx
@@ -1,9 +1,13 @@
-import { type FC } from 'react';
+import { useEffect, type FC } from 'react';
 import { useFetch } from '../../hooks';
 import { TChannels, TChannelsProps } from './Channels.types';
 
 export const Channels: FC<TChannelsProps> = ({ setCurrentChannelId }) => {
   const { data, isLoading } = useFetch<TChannels[]>({ url: '/channels/userChannels' });
+
+  useEffect(() => {
+    data && data.length > 0 && setCurrentChannelId(data[0].id);
+  }, [data, setCurrentChannelId]); // Set the first channel as the initial channel
 
   const channels = data?.map((channel) => (
     <li onClick={() => setCurrentChannelId(channel.id)} key={channel.id}>
