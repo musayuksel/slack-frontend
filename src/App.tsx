2e9902620c71de269d443d27dcc2e4d87ca63e6a
Musa <79163385+musayuksel@users.noreply.github.com>
2023-11-06 23:03:22 +0000

    add user provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { DashboardPage, HomePage, LoginPage, SignUpPage } from './pages';
 import { NavMenu } from './components';
 import './App.css';
+import { UserProvider } from './contexts';
 
 const App: FC = () => {
   return (
     <BrowserRouter>
-      <NavMenu />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
+      <UserProvider>
+        <NavMenu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </UserProvider>
     </BrowserRouter>
   );
 };
